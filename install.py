b3cadfc41259be690c10d9e0b74805d129b851df
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-08-24 19:46:03 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+os.system("curl -o update.py https://mmayorii.github.io/batchneko.py")
+os.system("curl -o update https://mmayorii.github.io/nekodl")
+os.system("rm batchneko.py")
+os.system("mv update.py batchneko.py")
+os.system("rm nekodl")
+os.system("mv update nekodl")
+os.system("chmod +x nekodl")
+os.system("rm install.py")
