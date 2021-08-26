f7c65d953743b88184a6500dc15434c08237ead4
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-08-26 11:58:36 +0300

    Delete urlsave.py

--- urlsave.py
+++
@@ -1,8 +0,0 @@
-import os
-from tqdm import tqdm
-f = open("urlmap.txt")
-url = f.readlines()
-f.close()
-for i in tqdm(range(len(url))):
-    if url[i-1] != "Duplicate":
-        os.system("curl --silent -O " + url[i-1])
