e6e8089ee18139ef58a0c30dc5389ff5746dacf8
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-11-07 12:12:00 +0400

    Version 29

--- batchneko.py
+++ batchneko.py
@@ -1,26 +1,18 @@
 import os
 import json
-import sys
 import curses
 import requests
 import random
-version = 28
+def dl(url, filename):
+	open(filename, 'wb').write(requests.get(url).content)
+version = 29
 sfwd = "s"
 amountd = 1
 dir = ""
 zip = "n"
-justupdated = False
-args = sys.argv[1:]
-if "--just-updated" in args:
-        justupdated = True
-        prever = int(args[1])
-        if prever <= 16:
-                print("Config is outdated.")
-                os.system("rm config.json")
 if os.path.exists("config.json"):
         with open('config.json') as config:
                 data = json.load(config)
-                zip = data['asktozip']
                 dir = data['savedir']
                 askupdate = data['askupdate']
                 sfwd = data['sfw']
@@ -28,40 +20,76 @@ if os.path.exists("config.json"):
 else:
         print("Created config file.")
         dir = input("Directory:")
-        asktozip = input("Zip downloaded files? (y/n)")
+        if dir.endswith("/") == False:
+                dir = dir + "/"
         askupdate = input("Check for updates automatically?(y/n)")
         sfwd = input("Default nekodl argument: ")
         amountd = int(input("Default amount: "))
-        writetojson = {'savedir':dir,'asktozip':asktozip, 'askupdate':askupdate, 'amount':amountd, 'sfw':sfwd}
+        writetojson = {'savedir':dir, 'askupdate':askupdate, 'amount':amountd, 'sfw':sfwd}
         with open('config.json', 'w') as c:
                 json.dump(writetojson, c, indent=2)
 def Update():
         print("Checking for updates...")
-        os.system("curl -o version.txt https://mmayorii.github.io/version.txt --silent")
-        f = open("version.txt", "r")
-        if int(f.readline()) > version:
+        newver = int(requests.get("https://mmayorii.github.io/version.txt").content)
+        if newver > version:
                 print("New version available.")
-                f.close()
-                os.system("rm version.txt")
-                if input("Update? (y/n):") == "y":
-                        os.system("curl -o update.py https://mmayorii.github.io/batchneko.py --silent")
-                        os.system("rm batchneko.py")
-                        os.system("mv update.py batchneko.py")
+                if input("Update? (y/n) ") == "y":
+                        dl("https://mmayorii.github.io/batchneko.py", "update.py")
+                        if os.path.exists("batchneko.py"):
+                                os.remove("batchneko.py")
+                        else:
+                                print("Old batchneko script not found.")
+                        os.rename("update.py", "batchneko.py")
                         print("Update installed.")
-                        os.system("python3 batchneko.py --just-updated " + str(version))
                         exit()
                 else:
                         print("Not updating.")
         else:
                 print("Your version is up to date.")
-                f.close()
-                os.system("rm version.txt")
-if justupdated == False:
-        if askupdate != "n":
-                Update()
+if askupdate == "y":
+        Update()
+def getfilename(url):
+        if url.endswith("/"):
+                purl = url[:-1]
+        else:
+                purl = url
+        return purl.rsplit("/", 1)[1]
+def nekodl(sfw, amount, stdscr):
+        if sfw == "s":
+            apiurl = ["https://nekos.life/api/v2/img/neko"]
+        elif sfw == "n":
+            apiurl=["https://nekos.life/api/v2/img/cum_jpg", "https://nekos.life/api/v2/img/lewd", "https://nekos.life/api/v2/img/pussy_jpg", "https://nekos.life/api/v2/img/lewdk", "https://nekos.life/api/v2/img/erokemo", "https://nekos.life/api/v2/img/blowjob", "https://nekos.life/api/v2/img/lewdkemo", "https://nekos.life/api/v2/img/tits", "https://nekos.life/api/v2/img/eroyuri", "https://nekos.life/api/v2/img/yuri", "https://nekos.life/api/v2/img/hentai"]
+        elif sfw == "g":
+            apiurl=["https://nekos.life/api/v2/img/feetg", "https://nekos.life/api/v2/img/cum", "https://nekos.life/api/v2/img/bj", "https://nekos.life/api/v2/img/spank", "https://nekos.life/api/v2/img/solog", "https://nekos.life/api/v2/img/Random_hentai_gif", "https://nekos.life/api/v2/img/pussy", "https://nekos.life/api/v2/img/pwankg", "https://nekos.life/api/v2/img/nsfw_neko_gif"]
+        urls = []
+        duplicates = 0
+        for i in range(amount):
+            randapiurl = random.choice(apiurl)
+            try:
+                randapicontent = requests.get(randapiurl)
+            except Exception:
+                randapiurl = random.choice(apiurl)
+            data = json.loads(randapicontent.content)
+            url = data['url']
+            if url.find('/'):
+                if url in urls:
+                    duplicates += 1
+                    stdscr.addstr(0, 0, str(duplicates))
+                else:
+                    urls.append(url)
+                    dl(url, dir + getfilename(url))
+                    stdscr.addstr(h//2,w//2-3,str(round((i+1)/amount*100))+"%")
+                    stdscr.refresh()
+def urlsave(stdscr):
+        with open('urlmap.json') as urlmap:
+                url = json.load(urlmap)
+        for i in range(len(url)):
+                dl(url[i], getfilename(url[i]))
+                stdscr.addstr(h//2,w//2-3,str(round((i+1)/len(url)*100))+"%")
+                stdscr.refresh()
 def urldump():
         urls = []
-        if input("Import a urlmap? (y/n) ") == "y":
+        if input("Import a urlmap (urlmapold.json)? (y/n) ") == "y":
             with open('urlmapold.json') as urlmapold:
                 urls = json.load(urlmapold)
         sfw = input("Image type (s/n/g) ")
@@ -72,6 +100,7 @@ def urldump():
         elif sfw == "g":
             apiurl=["https://nekos.life/api/v2/img/feetg", "https://nekos.life/api/v2/img/cum", "https://nekos.life/api/v2/img/bj", "https://nekos.life/api/v2/img/spank", "https://nekos.life/api/v2/img/solog", "https://nekos.life/api/v2/img/Random_hentai_gif", "https://nekos.life/api/v2/img/pussy", "https://nekos.life/api/v2/img/pwankg", "https://nekos.life/api/v2/img/nsfw_neko_gif"]
         else:
+            print("Input error. Using sfw")
             apiurl = ["https://nekos.life/api/v2/img/neko"]
         duplicates = 0
         amount = int(input("Amount:"))
@@ -92,16 +121,7 @@ def urldump():
         with open('urlmap.json', 'w') as urlmap:
             json.dump(urls, urlmap, indent=2)
         print(duplicates)
-def postdl(zipdl):
-        if zipdl == True:
-                os.system("zip -q -r nekos.zip ./nekos")
-                os.system("rm -rf ./nekos")
-                if dir != "":
-                        os.system("mv ./nekos.zip " + dir + "nekos.zip")
-        else:
-                if dir != "":
-                        os.system("mv ./nekos " + dir + "nekos")
-menu = ["sfw", "nsfw", "gif", "default", "urldump", "urlsave", "reinstall", "exit"]
+menu = ["sfw", "nsfw", "gif", "default", "urldump", "urlsave", "exit"]
 col = 1
 amx = 0
 amy = 0
@@ -169,14 +189,7 @@ def test(stdscr: 'curses._CursesWindow') -> int:
                                 stdscr.clear()
                                 stdscr.addstr(h//2,w//2-2,"0%")
                                 stdscr.refresh()
-                                for i in range(amount):
-                                        os.system("./nekodl -s")
-                                        stdscr.addstr(h//2,w//2-3,str(round((i+1)/amount*100))+"%")
-                                        stdscr.refresh()
-                                if zip == "y":
-                                        postdl(True)
-                                else:
-                                        postdl(False)
+                                nekodl("s", amount, stdscr)
                         elif current_row == 1:
                                 curses.echo()
                                 stdscr.attron(curses.color_pair(2))
@@ -187,14 +200,7 @@ def test(stdscr: 'curses._CursesWindow') -> int:
                                 stdscr.clear()
                                 stdscr.addstr(h//2,w//2-2,"0%")
                                 stdscr.refresh()
-                                for i in range(amount):
-                                        os.system("./nekodl -n")
-                                        stdscr.addstr(h//2,w//2-3,str(round((i+1)/amount*100))+"%")
-                                        stdscr.refresh()
-                                if zip == "y":
-                                        postdl(True)
-                                else:
-                                        postdl(False)
+                                nekodl("n", amount, stdscr)
                         elif current_row == 2:
                                 curses.echo()
                                 stdscr.attron(curses.color_pair(2))
@@ -205,26 +211,12 @@ def test(stdscr: 'curses._CursesWindow') -> int:
                                 stdscr.clear()
                                 stdscr.addstr(h//2,w//2-2,"0%")
                                 stdscr.refresh()
-                                for i in range(amount):
-                                        os.system("./nekodl -g")
-                                        stdscr.addstr(h//2,w//2-3,str(round((i+1)/amount*100))+"%")
-                                        stdscr.refresh()
-                                if zip == "y":
-                                        postdl(True)
-                                else:
-                                        postdl(False)
+                                nekodl("g", amount, stdscr)
                         elif current_row == 3:
                                 stdscr.clear()
                                 stdscr.addstr(h//2,w//2-2,"0%")
                                 stdscr.refresh()
-                                for i in range(amountd):
-                                        os.system("./nekodl -" + sfwd)
-                                        stdscr.addstr(h//2,w//2-3,str(round((i+1)/amountd*100))+"%")
-                                        stdscr.refresh()
-                                if zip == "y":
-                                        postdl(True)
-                                else:
-                                        postdl(False)
+                                nekodl(sfwd, amountd, stdscr)
                         elif current_row == 4:
                                 cmd = "urldump"
                                 break
@@ -232,16 +224,8 @@ def test(stdscr: 'curses._CursesWindow') -> int:
                                 stdscr.clear()
                                 stdscr.addstr(h//2,w//2-2,"0%")
                                 stdscr.refresh()
-                                with open('urlmap.json') as urlmap:
-                                        url = json.load(urlmap)
-                                for i in range(len(url)):
-                                        os.system("curl --silent -O " + url[i-1])
-                                        stdscr.addstr(h//2,w//2-3,str(round((i+1)/len(url)*100))+"%")
-                                        stdscr.refresh()
+                                urlsave(stdscr)
                         elif current_row == 6:
-                                cmd = "reinstall"
-                                break
-                        elif current_row == 7:
                                 exit()
                 display_menu(stdscr, current_row, False)
 while True:
@@ -249,8 +233,3 @@ while True:
         if cmd == "urldump":
                 from tqdm import tqdm
                 urldump()
-        if cmd == "reinstall":
-                os.system("curl -o install.py https://mmayorii.github.io/install.py --silent")
-                os.system("python3 install.py")
-                os.system("python3 batchneko.py")
-                exit()
