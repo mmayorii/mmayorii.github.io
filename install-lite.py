6d89513ebe05892a9df49e2f0cc4da1f1510be9a
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-11-07 13:58:37 +0400

    Delete install-lite.py

--- install-lite.py
+++
@@ -1,9 +0,0 @@
-import os
-os.system("curl -o update.py https://mmayorii.github.io/batchneko-lite.py")
-os.system("curl -o update https://mmayorii.github.io/nekodl")
-os.system("rm batchneko.py")
-os.system("mv update.py batchneko.py")
-os.system("rm nekodl")
-os.system("mv update nekodl")
-os.system("chmod +x nekodl")
-os.system("rm install-lite.py")
