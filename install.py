c44f18b7f8729136d8da6e9b92a1b6e11a223904
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-11-07 14:03:16 +0400

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,10 @@
 import requests
-open("batchneko.py", "wb").write(requests.get("https://mmayorii.github.io/batchneko.py").content)
+type = input("Install normal or lite batchneko version? (n/l) ")
+if type == "n":
+  open("batchneko.py", "wb").write(requests.get("https://mmayorii.github.io/batchneko.py").content)
+elif type == "l":
+  open("batchneko.py", "wb").write(requests.get("https://mmayorii.github.io/batchneko-lite.py").content)
+else:
+  print("Input error.")
+  exit()
 print("Installed")
