b62fc7f66b189c017769e9c5a56f59335f440752
mmayorii <89463174+mmayorii@users.noreply.github.com>
2021-11-09 14:03:53 +0400

    Add files via upload

--- install.py
+++ install.py
@@ -1,9 +1,9 @@
 import requests
 type = input("Install normal or lite batchneko version? (n/l) ")
 if type == "n":
-  open("batchneko.py", "wb").write(requests.get("https://mmayorii.github.io/batchneko.py").content)
+  open("batchneko.py", "wb").write(requests.get("https://phil213314.github.io/batchneko/batchneko.py").content)
 elif type == "l":
-  open("batchneko.py", "wb").write(requests.get("https://mmayorii.github.io/batchneko-lite.py").content)
+  open("batchneko.py", "wb").write(requests.get("https://phil213314.github.io/batchneko/batchneko-lite.py").content)
 else:
   print("Input error.")
   exit()
